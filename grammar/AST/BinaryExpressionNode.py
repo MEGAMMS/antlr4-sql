a0e438d810f7077d88a6d097dc7a0cf8cb4d26d6
TariqSp <teto75928@gmail.com>
2026-01-21 09:20:14 +0300

    add exp classes

---
+++ grammar/AST/BinaryExpressionNode.py
@@ -0,0 +1,13 @@
+from grammar.AST import ExpressionNode
+
+
+class BinaryExpressionNode(ExpressionNode):
+    def __init__(self, operator, left, right):
+        self.operator = operator
+        self.left = left
+        self.right = right
+
+    def print(self, indent=0):
+        print(f"{self._indent(indent)}BinaryExpr ({self.operator})")
+        self.left.print(indent + 1)
+        self.right.print(indent + 1)
